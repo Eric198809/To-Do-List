e554a75bde6b5b3dfd48b7b640ba72c4a00af0b2
Eric Besson <150351199+Eric198809@users.noreply.github.com>
2024-07-11 16:42:11 +0200

    ok

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 const form = document.querySelector("form");
 
+// Stocker dans le local storage
+const storeList = () => {
+  window.localStorage.todoList = list.innerHTML;
+};
+
+const getTodos = () => {
+  if (window.localStorage.todoList) {
+    list.innerHTML = window.localStorage.todoList;
+  } else {
+    list.innerHTML = `<li> Créez votre To Do List </li>`;
+  }
+};
+
+// Charger le contenu du Local Storage au lancement de la page
+window.addEventListener("load", getTodos);
+
+// Ajouter une ToDo
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   list.innerHTML += `<li>${item.value} </li> `;
   item.value = "";
+  storeList();
 });
 
+// Supprimer une Todo
 list.addEventListener("click", (e) => {
   if (e.target.classList.contains("checked")) {
-    e.target.remove()
-  }else{
-    e.target.classList.add("checked")
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
   }
+  storeList();
 });
