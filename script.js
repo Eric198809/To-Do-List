fcfc077aa22f7ee61e53022aca57e7a05e8ddf56
Eric Besson <150351199+Eric198809@users.noreply.github.com>
2024-07-11 16:17:28 +0200

    ajout creation et supp d'une todo

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const form = document.querySelector("form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  list.innerHTML += `<li>${item.value} </li> `;
+  item.value = "";
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove()
+  }else{
+    e.target.classList.add("checked")
+  }
+});
